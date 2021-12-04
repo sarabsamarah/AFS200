b1f07a320e0598124213916f5cd31eaf6bdb2146
sarabsamarah <sarab.samarah@bryanuniversity.edu>
2021-12-04 09:04:19 -0800

    odd even project

---
+++ week2/oddEven.py
@@ -0,0 +1,7 @@
+number = int(input("Enter a number: "))
+if (number % 2) == 0:
+   print("Your number is Even")
+else:
+   print("Your number is odd")
+if (number % 4) == 0:
+    print("Your number is a multiple of four")
